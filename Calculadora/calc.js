3c8caf623438b2af16de75b140cccc55f858b536
Danilo <daniloaraujomota@gmail.com>
2024-03-03 09:49:15 -0300

    first commit

---
+++ Calculadora/calc.js
@@ -0,0 +1,7 @@
+var SomaFunc = require('./somar');
+var DivFunc = require('./dividir');
+var MultiFunc = require('./multiplicar');
+var Subfunc = require('./subtrair');
+
+
+console.log(SomaFunc(2,3));
