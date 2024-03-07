e5eae99d4520438d823533d8ed47216b8112303c
Danilo <daniloaraujomota@gmail.com>
2024-03-07 11:12:04 -0300

    .

--- Express/index.js
+++ Express/index.js
@@ -1,38 +1,33 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const handlebars  = require('express-handlebars');
 const Sequelize = require('sequelize');
 
 // Config
-const app = express();
-const PORT = 8081;
+    const app = express();
+    const PORT = 8081;
 
-// Template engine
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+    // Template engine
+    app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+    app.set('view engine', 'handlebars');
 
-// Connection to the database
-const sequelize = new Sequelize('teste', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+    // Connection to the database
+    const sequelize = new Sequelize('teste', 'root', 'root', {
+        host: 'localhost',
+        dialect: 'mysql'
+    });
+    sequelize.authenticate().then(()=>{
+        console.log('autenticado com sucesso!')
+    }).catch(function(erro){
+        console.log(erro)
+    })
 
-// Define Sequelize models
-const User = sequelize.define('user', {
-    // Your model fields here
-    username: Sequelize.STRING,
-    email: Sequelize.STRING
-});
 
-// Sync Sequelize models with the database
-sequelize.sync().then(() => {
-    console.log('Database and tables synced');
-}).catch((err) => {
-    console.error('Error syncing database:', err);
-});
+
 
 // Routes
-// Define your routes here
+app.get('/cadastro',(req,res)=>{
+    res.render('formulario.handlebars')
+})
 
 // Start the server
 app.listen(PORT, () => {
